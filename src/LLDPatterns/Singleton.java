9328119017babe2bde64077ba62cbc3c36cdd07e
jivesh28 <jivu.b31514@gmial.com>
2026-03-25 00:11:59 +0530

    learnt strategy design pattern it is a part of creational pattern

--- src/LLDPatterns/Singleton.java
+++ src/LLDPatterns/Singleton.java
@@ -1,4 +1,26 @@
 package LLDPatterns;
-
+// only one instance throughout the repo, used for DB pool, logging etc
 public class Singleton {
+//
+    // the constructor must be private
+    private Singleton(){
+        // so no  object can be created
+    };
+
+    private static class helper{
+        private final static Singleton Instance = new Singleton();
+    }
+    public static Singleton GetInstance() {
+
+//            if (INSTANCE == null) {
+//                synchronized (Singleton.class)
+//                {
+//                    if (INSTANCE == null) {
+//                        INSTANCE = new Singleton();
+//                    }
+//                }
+//            }
+        return helper.Instance;
+    }
 }
+
